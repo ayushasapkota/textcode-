491f675afbd14fb571b8f63e1183553be333cf8b
unknown <ayushasapkota108@gmail.com>
2025-02-12 12:22:07 +0545

    Add : Firt project

---
+++ votingsystem.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+int main() {
+    
+    int age;
+    char citizen;
+    
+    printf("2";);
+    scanf("%d, &age");
+    
+    printf("Are you a citizen?(yes/no): ");
+    scanf (" %c", &citizen):
+
+    if (age >-18&& (citizen -- 'y' || citizen -- 'y')) {
+    printf(you are eligible to vote.\n"): 
+        
